44c03c38e1f6d597c726fb2453d92261d6a300bd
Bastian Waidelich <bastian@typo3.org>
2012-01-16 15:54:46 +0100

    [FEATURE] Make RenderViewHelper more flexible
    - Add arguments $presetName and $overrideConfiguration to RenderViewHelper
    - Rename "facebook" preset to "Facebook" and fix settings
       (it's currently not allowed to override implementationClassName of the base FormFieldDefinition)
    - Move "FormNavigation" partial to "Form/Navigation"

--- Classes/ViewHelpers/RenderViewHelper.php
+++ Classes/ViewHelpers/RenderViewHelper.php
@@ -25,13 +25,16 @@ use TYPO3\FLOW3\Annotations as FLOW3;
  */
 class RenderViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 
-	public function render($factoryClass) {
+	/**
+	 * @param string $factoryClass The fully qualified class name of the factory (which has to implement \TYPO3\Form\Factory\FormFactoryInterface)
+	 * @param string $presetName name of the preset to use
+	 * @param array $overrideConfiguration factory specific configuration
+	 * @return string the rendered form
+	 */
+	public function render($factoryClass, $presetName = 'Default', array $overrideConfiguration = array()) {
 		$factory = $this->objectManager->get($factoryClass);
-
-		$formDefinition = $factory->build(array(), 'Default');
-
+		$formDefinition = $factory->build($overrideConfiguration, $presetName);
 		$form = $formDefinition->bind($this->controllerContext->getRequest());
-
 		return $form->render();
 	}
 }
