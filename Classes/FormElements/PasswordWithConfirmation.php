6366bbd2ee7aa6db7794e9cc6a54021bb3b8b6b7
Aske Ertmann <aske@moc.net>
2012-04-25 14:16:14 +0200

    [FEATURE] New element "Password with confirmation"
    This adds a new FormElement "TYPO3.Form:PasswordWithConfirmation"
    that displays two password fields.
    
    Technical details:
    The element checks whether both fields are equal and add a validation error
    to the corresponding processing rule if that's not the case.
    Otherwise the element value is changed to the password so that additional
    regular validators can be used to enforce length and complexity of the
    password.
    
    Change-Id: I80c68bdd04b89eaa9c4687798291246a0e88123f

---
+++ Classes/FormElements/PasswordWithConfirmation.php
@@ -0,0 +1,30 @@
+<?php
+namespace TYPO3\Form\FormElements;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3.Form".                 *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * A password with confirmation form element
+ */
+class PasswordWithConfirmation extends \TYPO3\Form\Core\Model\AbstractFormElement {
+
+	public function onSubmit(\TYPO3\Form\Core\Runtime\FormRuntime $formRuntime, &$elementValue) {
+		if ($elementValue['password'] !== $elementValue['confirmation']) {
+			$processingRule = $this->getRootForm()->getProcessingRule($this->getIdentifier());
+			$processingRule->getProcessingMessages()->addError(new \TYPO3\FLOW3\Error\Error('Password doesn\'t match confirmation', 1334768052));
+		}
+		$elementValue = $elementValue['password'];
+	}
+
+}
+?>
