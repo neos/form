e296d50c9702668c88ddaad2ce8444ef08c14ac8
Karsten Dambekalns <karsten@typo3.org>
2013-03-28 10:45:27 +0100

    [TASK] Remove leading backslash in use statement(s)
    Change-Id: I01c2e05ae6abf70ca478a5ac4f247fbb6b4e6bb7

--- Classes/TYPO3/Form/Core/Model/FinisherContext.php
+++ Classes/TYPO3/Form/Core/Model/FinisherContext.php
@@ -11,7 +11,7 @@ namespace TYPO3\Form\Core\Model;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-use \TYPO3\Form\Core\Runtime\FormRuntime;
+use TYPO3\Form\Core\Runtime\FormRuntime;
 
 /**
  * The context that is passed to each finisher when executed.
--- Classes/TYPO3/Form/Core/Model/FinisherInterface.php
+++ Classes/TYPO3/Form/Core/Model/FinisherInterface.php
@@ -11,7 +11,7 @@ namespace TYPO3\Form\Core\Model;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-use \TYPO3\Form\Core\Model\FinisherContext;
+use TYPO3\Form\Core\Model\FinisherContext;
 
 /**
  * Finisher that can be attached to a form in order to be invoked
